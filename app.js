ea26bc4ab30dccb8dd3db4f0a0d84847340436b8
shivayogids <shivayogids5@gmail.com>
2026-03-14 20:20:59 +0530

    added in new way

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ let btn = document.querySelector("button");
 let input = document.querySelector("input");
 let ul = document.querySelector("ul");
 
-btn.addEventListener("click", function () {
-    let task = input.value;
-
-    let li = document.createElement("li");
-    li.innerText = task;
-
-    ul.appendChild(li);
-
+btn.addEventListener("click",function(){
+    let item = document.createElement("li");
+    item.innerText = input.value;
+    ul.appendChild(item);
     input.value = "";
-});
+})
